59df550e5ab2edd23be34dff626de52df215e0f7
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-14 19:54:03 +0000

    Add files via upload

---
+++ GasMilageTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class GasMilageTest
+{
+
+    
+    static void Main()
+    {
+        
+        GasMilage gasMilage = new GasMilage();
+
+        int counter = 0;
+        
+
+        Console.Write("\nPlease enter the amount of calculations you like to perform: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        while (counter < limit)
+        {
+            Console.Write("\nPlease enter the amount of miles driven: ");
+            int gasMilagetheMilesDriven = int.Parse(Console.ReadLine());
+            gasMilage.SetMilesDriven(gasMilagetheMilesDriven);
+
+            Console.Write("Please enter the amount of gallons your vehicle used: ");
+            int gasMilagetheGallons = int.Parse(Console.ReadLine());
+            gasMilage.SetGallons(gasMilagetheGallons);
+
+            Console.WriteLine($"The average fuel consuption is: {Math.Round(((double)gasMilage.GetMilesDriven() / (double)gasMilage.GetGallons()), 2)} ");
+
+            ++counter;
+
+        }
+       
+
+    }
+}
+
